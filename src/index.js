0272cf150bf6bafeaf3f1eb4f685fec85f5e3675
wainervega <wainervegar@gmail.com>
2022-10-20 01:15:33 -0500

    Corrección3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';ñ
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
