cf75c2802f25c9f5513cf66fb8fd37554f46675a
wainervega <wainervegar@gmail.com>
2022-10-20 04:45:03 -0500

    Prueba3

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,6 +1,6 @@
-import React, { useDebugValue, useState } from 'react'
+import React, { useDebugValue, useState, useEffect } from 'react'
 import {db} from './firebase';//'db'
-import { collection, addDoc, deleteDoc, doc, OnSnapShot, updateDoc } from 'firebase/firestore';
+import { collection, addDoc, deleteDoc, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 
 const Formulario = () => {
     const [fruta, setFruta] = useState ('')
@@ -8,6 +8,28 @@ const Formulario = () => {
     const [listaFrutas, setListaFrutas] = useState ([])
     const [modoEdicion, setModoEdicion] = useState (false)
     const [id, setId] = useState ('')
+
+
+    useEffect (()=>{
+        const obtenerDatos = async () => {
+            try{
+                await onSnapshot (collection(db, "frutas"), (query)=>{
+                    setListaFrutas(query.docs.map((doc)=>({...doc.data(), id:doc.id})))
+                })
+            }catch(error){
+                console.log(error)
+            }
+        }
+        obtenerDatos ();
+    }, [])
+
+    const eliminar = async id=>{
+        try{
+            await deleteDoc(doc(db, 'frutas', id))
+        }catch(error){
+            console.log(error)
+        }
+    }
 
     const guardarFrutas = async(e) => {
         e.preventDefault ()
@@ -29,6 +51,44 @@ const Formulario = () => {
         }
     }
 
+    const editar = item =>{
+        setFruta (item.nombreFruta)
+        setDescripcion(item.nombreDescripcion)
+        setId(item.id)
+        setModoEdicion(true)
+    }
+
+    const editarFrutas = async (e) => {
+        e.preventDefault()
+        try{
+            const docRef = doc(db, 'frutas', id);
+            await updateDoc(docRef, {
+                nombreFruta:fruta,
+                nombreDescripcion:descripcion
+            })
+
+            const nuevoArray = listaFrutas.map(
+                item => item.id === id ? {id: id, nombreFruta:fruta, nombreDescripcion:descripcion} : item
+            )
+            
+            setListaFrutas(nuevoArray)
+            setFruta('')
+            setDescripcion('')
+            setId('')
+            setModoEdicion(false)
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const cancelar = () =>{
+        setModoEdicion(false)
+        setFruta ('')
+        setDescripcion ('')
+        setId ('')
+    }
+
     return (
         <div className='container mt-5'>
             <h1 className='text-center'>ACTIVIDAD 20%</h1>
@@ -38,7 +98,15 @@ const Formulario = () => {
                     <h4 className="text-center">Listado de Frutas</h4>
                     <ul className="list-group">
                         {
-
+                            listaFrutas.map(item => (
+                                <li className= "list-group-item" key={item.id}>
+                                    <span className="lead">{item.nombreFruta} - {item.nombreDescripcion}</span>
+                                    <button className="btn btn-danger btn=sm float-end mx-2"
+                                    onClick={()=>eliminar(item.id)}>Eliminar</button>
+                                    <button className="btn btn-warning btn=sm float-end mx-2"
+                                    onClick={()=>editar(item)}>Editar</button>
+                                </li>
+                            ))
                         }
                     </ul>
                 </div>
@@ -48,9 +116,11 @@ const Formulario = () => {
 
             <div className="col-4">
                 <h4 className="text-center">
-                    Agregar Frutas
+                    {
+                        modoEdicion ? 'Editar Frutas' : 'Agregar Frutas'
+                    }
                 </h4>
-                <form onSubmit="guardarFrutas">
+                <form onSubmit={modoEdicion ? editarFrutas : guardarFrutas}>
                     <input type="text" 
                     Classname="form-control mb-2" 
                     placeholder='Ingrese Fruta'
@@ -62,12 +132,28 @@ const Formulario = () => {
                     value={descripcion} 
                     onChange= {(e)=>setDescripcion(e.target.value)}/>
 
+                    {
+                        modoEdicion ?
+                        (
+                            <>
+                                <button
+                                className='btn btn-warning btn-block'
+                                on='submit'>Editar</button>
+                                <button
+                                className='btn btn-dark btn-block mx-2'
+                                onClick= {()=>cancelar()}>Cancelar</button>
+                            </>
+                        )
+                        :
+                        
                     <button 
                         type='submit'
-                        className='btn btn-primary btn-lock'>
+                        className='btn btn-primary btn-block'>
                         Agregar
                     </button>
+                    }
                 </form>
+                
             </div>
             </div>
 
